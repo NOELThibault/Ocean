ce195908e5bdc06e92625818eaebef27501afb1c
Thibault NOEL <thibault.noel0907@gmail.com>
2024-06-05 17:36:24 +0200

    Minimal project
    Imported necessary libraries and created base window

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,54 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <debug.hpp>
+#include <iostream>
+
+void input( GLFWwindow * window )
+{
+    if( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
+        glfwSetWindowShouldClose( window, true );
+}
 
 int main()
 {
+    // Initialize GLFW
+    if( !glfwInit() )
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    // Set OpenGL version to 3.3
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+
+    // Create window
+    GLFWwindow * window = glfwCreateWindow( 800, 600, "Ocean", glfwGetPrimaryMonitor(), nullptr );
+    if( !window )
+    {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent( window );
+    
+    // Initialize GLAD    
+    if( !gladLoadGLLoader( ( GLADloadproc ) glfwGetProcAddress ) )
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }  
+
+    while( !glfwWindowShouldClose( window ) )
+    {
+        glClear( GL_COLOR_BUFFER_BIT );
+        input( window );
+        glfwSwapBuffers( window );
+        glfwPollEvents();
+    }
+
+    glCheckError();
+    glfwDestroyWindow( window );
+    glfwTerminate();
     return 0;
 }
